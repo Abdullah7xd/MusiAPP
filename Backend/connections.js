db2a16fdd9a5574b94b61b640f50e8698707e4c1
ashutoshshubham <ashutoshshubham29@gmail.com>
2023-06-24 13:48:48 +0530

    model and router added

--- Backend/connections.js
+++ Backend/connections.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const url = 'mongodb+srv://<music123>:<music321>@cluster0.ihx3lfc.mongodb.net/music-app?retryWrites=true&w=majority';
+// const url = 'mongodb+srv://music123:music321@cluster0.ihx3lfc.mongodb.net/music-app?retryWrites=true&w=majority';
+const url = 'mongodb+srv://Abdullah:Abdullah123@cluster0.ihx3lfc.mongodb.net/musicApp?retryWrites=true&w=majority';
 
 
 mongoose.connect(url)
--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 
+const userRouter = require('./routers/userRouter')
+
+
+const cors = require('cors')
+
+
 const app=express();
 
-app.get("/",(req,resp)=>{
-resp.send("app is working...")    
-});
+app.use(cors({
+    origin:['http://localhost:3000']
+}));
 
-app.listen(5000)
+app.use(express.json());                 
+app.use('/user', userRouter);
+
+
+
+// app.get("/",(req,res)=>{
+// res.send("app is working...")    
+// });
+
+app.listen(5000, () => {console.log("server started")})
